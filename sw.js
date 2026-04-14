b494f7fe3be016ad6a10d4cbafbbac10e19666c8
abu-hisan <bidpendikalmatuq@gmail.com>
2026-04-14 10:37:05 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,13 @@
-const CACHE_NAME = 'Planner V.2';
-// Daftar file dan library yang harus didownload agar bisa offline
+const CACHE_NAME = 'dompetku-cache-v1';
 const urlsToCache = [
-  './',
   './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap',
-  'https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.3.4/b-3.2.5/b-html5-3.2.5/r-3.0.7/datatables.min.css',
-  'https://cdn.jsdelivr.net/npm/sweetalert2@11',
-  'https://code.jquery.com/jquery-3.7.1.min.js',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
-  'https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.3.4/b-3.2.5/b-html5-3.2.5/r-3.0.7/datatables.min.js',
-  'https://unpkg.com/dexie/dist/dexie.js' // Database offline
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -32,8 +17,8 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Kembalikan file dari cache (offline) jika ada, jika tidak ambil dari internet
-        return response || fetch(event.request);
+        if (response) { return response; }
+        return fetch(event.request);
       })
   );
 });
